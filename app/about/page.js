817b90465243978620a9e13fce46bfbb0e01df1e
Beth Howell <bethhowell117@gmail.com>
2025-03-28 17:41:38 -0400

    testing

--- app/about/page.js
+++ app/about/page.js
@@ -15,16 +15,16 @@ function SkillCard({name, src}){
 
 function AboutPage() {
   const skills = [
-    { name: 'JavaScript', image: '/javascript.svg'},
-    { name: 'React', image: '/react.svg'},
-    { name: 'Redux', image: '/redux.svg'},
-    { name: 'NextJS', image: '/next.svg'},
-    { name: 'React Native', image: '/react.svg'},
-    { name: 'AWS', image: '/aws.svg'},
-    { name: 'Python', image: '/python.svg'},
-    { name: 'MongoDB', image: '/mongodb.svg'},
-    { name: 'Microsoft Copilot', image: '/copilot.svg'},
-    { name: 'Node', image:'/node.svg'},
+    { name: 'JavaScript', image: '/images/javascript.svg'},
+    { name: 'React', image: '/images/react.svg'},
+    { name: 'Redux', image: '/images/redux.svg'},
+    { name: 'NextJS', image: '/images/next.svg'},
+    { name: 'React Native', image: '/images/react.svg'},
+    { name: 'AWS', image: '/images/aws.svg'},
+    { name: 'Python', image: '/images/python.svg'},
+    { name: 'MongoDB', image: '/images/mongodb.svg'},
+    { name: 'Microsoft Copilot', image: '/images/copilot.svg'},
+    { name: 'Node', image:'/images/node.svg'},
   ]
   
   return (
@@ -44,7 +44,7 @@ function AboutPage() {
             {skills.slice(0,5).map((skill, index) => (
               //  <SkillCard key={index} name={skill.name} src={skill.src}/>
               <div key={index} className={styles['skills-item']}>
-                <img src={skill.image} alt={skill.name} className={styles.skillImage} width="500" height="300"/>
+                <Image src={skill.image} alt={skill.name} className={styles.skillImage} width={500} height={300}/>
                 <p className={styles['skills-item-name']}>{skill.name}</p>
               </div>
             ))}
